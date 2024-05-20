39671207a66a82fe971a8f534966e9c7a61ae6e7
Bigeus <viniciusmsimoes2003@gmail.com>
2024-05-20 13:41:42 -0300

    add published sdk on npm

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,10 +3,15 @@ import { RouterProvider } from "react-router-dom"
 import { ROUTES } from ".."
 import GlobalStyles from "../Core/GlobalStyles"
 import info from "../Core/Utils/info"
+import log, { feature } from "bigeus-sdk"
 
 export default function App() {
 
     useEffect(() => {
+
+        log();
+        feature();
+
         window.onunhandledrejection = function () {
             info({ description: 'Deu erro', title: 'Erro' })
         }
